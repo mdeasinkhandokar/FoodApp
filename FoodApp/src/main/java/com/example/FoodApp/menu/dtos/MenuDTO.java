d03e9c841a5fc6a7e562bfbfc124b767e50bae10
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-02-27 17:12:23 +0600

    menu entity dtos repositiory

--- FoodApp/src/main/java/com/example/FoodApp/menu/dtos/MenuDTO.java
+++ FoodApp/src/main/java/com/example/FoodApp/menu/dtos/MenuDTO.java
@@ -1,4 +1,45 @@
 package com.example.FoodApp.menu.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MenuDTO {
+
+
+    private Long id;
+
+    @NotBlank(message= "Name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message="price is required")
+    @Positive(message="price must be positive")
+    private BigDecimal price;
+    private String imageUrl;
+
+    @NotNull(message="category ID is required")
+    private Long categoryId;
+
+    private MultipartFile imageFile;
+
+    private List<ReviewDTO>reviews;
+
+
+
+
+
+
+
+
 }
---
+++ FoodApp/src/main/java/com/example/FoodApp/menu/entity/Menu.java
@@ -0,0 +1,56 @@
+package com.example.FoodApp.menu.entity;
+
+
+import com.example.FoodApp.category.entity.Category;
+import com.stripe.model.Review;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "menus")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Menu {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private  String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
+    private List<OrderItem>orderItems;
+
+
+    @OneToMany(mappedBy = "menu")
+    private List<Review> reviews;
+
+
+
+
+
+
+
+
+
+
+}
