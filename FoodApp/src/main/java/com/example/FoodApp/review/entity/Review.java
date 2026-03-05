4ada08a8511078db49bc6e31f95899eee84ae3cc
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-03-06 00:44:10 +0600

    Review entity dtos repository

---
+++ FoodApp/src/main/java/com/example/FoodApp/review/entity/Review.java
@@ -0,0 +1,55 @@
+package com.example.FoodApp.review.entity;
+
+
+import com.example.FoodApp.auth_users.entity.User;
+import com.example.FoodApp.menu.entity.Menu;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "reviews")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+
+
+    private Integer rating ;
+
+    @Column(columnDefinition = "TEXT")
+    private  String comment;
+
+    private LocalDateTime createdAt;
+
+    @Column(name="order_id")
+    private Long orderId;;
+
+    @ManyToOne
+    @JoinColumn(name="menu_id")
+    private Menu menu;
+
+
+
+
+
+
+
+
+
+}
