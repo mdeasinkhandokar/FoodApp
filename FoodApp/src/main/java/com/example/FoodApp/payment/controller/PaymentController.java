cf2c29a7cdd17d420236cd63f8e3c0dcbdc5129d
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-03-13 12:55:00 +0600

    payment controller service

---
+++ FoodApp/src/main/java/com/example/FoodApp/payment/controller/PaymentController.java
@@ -0,0 +1,50 @@
+package com.example.FoodApp.payment.controller;
+
+
+import com.example.FoodApp.payment.dtos.PaymentDTO;
+import com.example.FoodApp.payment.services.PaymentService;
+import com.example.FoodApp.response.Response;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/pay")
+    public ResponseEntity<Response<?>> initializePayment(@RequestBody @Valid PaymentDTO paymentRequest){
+        return ResponseEntity.ok(paymentService.initializePayment(paymentRequest));
+    }
+
+    @PutMapping("/update")
+    public void updateOrderAfterPayment(@RequestBody PaymentDTO paymentRequest){
+        paymentService.updatePaymentForOrder(paymentRequest);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response<List<PaymentDTO>>> getAllPayments(){
+        return ResponseEntity.ok(paymentService.getAllPayments());
+    }
+
+    @GetMapping("/{paymentId}")
+    public ResponseEntity<Response<PaymentDTO>> getPaymentById(@PathVariable Long paymentId){
+        return ResponseEntity.ok(paymentService.getPaymentById(paymentId));
+    }
+
+}
+
+
+
+
+
+
